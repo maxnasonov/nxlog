fb3976e088719cc9491f0ae047e39177ae7b8b08
Maxim Nasonov <maxim@nasonov.me>
2016-08-10 11:52:49 +0300

    remove default value for processors

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             'nxlog'
-version          '0.1.9'
+version          '0.1.10'
 
 description      'Installs and configures NXLog_ce on Linux or Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
