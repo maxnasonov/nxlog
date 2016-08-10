8982fad25b4d9c123c9cfa0175737d015dc6ac17
Maxim Nasonov <maxim@nasonov.me>
2016-08-10 10:53:49 +0300

    add processor attribute

--- resources/source.rb
+++ resources/source.rb
@@ -28,6 +28,7 @@ attribute :input_module, kind_of: String, default: 'im_file', required: true,
                                       im_mark im_mseventlog im_msvistalog
                                       im_null im_ssl im_tcp im_udp im_uds)
 attribute :destination, kind_of: [String, Array], default: :defaults
+attribute :processor, kind_of: [String, Array], default: :defaults
 attribute :input_type, kind_of: String
 attribute :exec, kind_of: [String, Array]
 attribute :flow_control, kind_of: [TrueClass, FalseClass]
