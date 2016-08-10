b481178889376297d02af154b03f0a02a05e9f04
Maxim Nasonov <maxim@nasonov.me>
2016-08-10 10:21:11 +0300

    fix typ

--- recipes/resources_from_attributes.rb
+++ recipes/resources_from_attributes.rb
@@ -77,7 +77,7 @@ symbolize_attrs(node['nxlog']['papertrails'] || {}).each_pair do |name, values|
   end
 end
 
-Dir["#{node.default['nxlog']['conf_dir']}/nxlog.conf.d/*")].each do |path|
+Dir["#{node.default['nxlog']['conf_dir']}/nxlog.conf.d/*"].each do |path|
   config_filename = Pathname.new(path).basename.to_s
   next if config_filenames.include?(config_filename)
   file path do
