898ac6f980190ff5e2de3560c57a74a89219e438
Maxim Nasonov <maxim@nasonov.me>
2016-08-10 09:33:53 +0300

    bump version

--- Berksfile
+++
@@ -1,9 +0,0 @@
-source 'https://supermarket.chef.io'
-
-metadata
-
-cookbook 'apt'
-cookbook 'yum'
-cookbook 'windows', '1.38.2'
-
-cookbook 'test', path: 'test/fixtures/cookbooks/test'
--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             'nxlog'
-version          '0.1.0'
+version          '0.1.2'
 
 description      'Installs and configures NXLog_ce on Linux or Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
